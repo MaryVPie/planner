5989a562be51f3a07a216a86e821b1399f774654
MaryVPie <pirogova.mariia@gmail.com>
2021-06-21 20:49:24 -0700

    add function to save button

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
+// shows header date and time
+function headerDate() {
+    var currentHeaderDate = moment().format('MMMM Do YYYY, h:mm a');
+    $("#currentDay").text(currentHeaderDate);
+}
+
+headerDate();
+
 //rows array
 var rowsPlanner= [
     {
         id: 1,
         wasPassed: false,
-        text: "9AM"
+        text: "9 AM"
     },
     
     {
         id: 2,
         wasPassed: false,
-        text: "10AM"
+        text: "10 AM"
     },
 
     {
         id: 3,
         wasPassed: false,
-        text: "11AM"
+        text: "11 AM"
     },
 
     {
         id: 4,
         wasPassed: false,
-        text: "12PM"
+        text: "12 PM"
     },
 
     {
         id: 5,
         wasPassed: false,
-        text: "1PM"
+        text: "1 PM"
     },
 
     {
@@ -55,106 +63,61 @@ var rowsPlanner= [
     }
 ]
 
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  }
 
 
+//clonnig and adding new rows with different id
+for (let idx = 0; idx < rowsPlanner.length; idx++) {
+    const rowData = rowsPlanner[idx];
+
+    var threeColumnsTable$ = $('#template').clone();
+
+    threeColumnsTable$.attr("id", rowData.id);
+
+//   threeColumnsTable$ how to remove class jquery
+    threeColumnsTable$.removeClass("hidden");
+    
+    // puts time in column 1. 
+    $("div:first-child", threeColumnsTable$).html(rowData.text);
+
+    $("div:nth-child(2)", threeColumnsTable$).addClass("future");
 
 
-// shows header date and time
-function headerDate() {
-    var currentHeaderDate = moment().format('MMMM Do YYYY, h:mm a');
-    $("#currentDay").text(currentHeaderDate);
+    // adds cloned row at the end of container
+    $(".container").append(threeColumnsTable$);
 }
 
-headerDate();
+//saves time and text
+function savings() {
 
-var threeColumnsTable$ = $('#template').clone();
-threeColumnsTable$.attr("id", uuidv4());
-var clonedTable$ = $(".container").append(threeColumnsTable$);
+    debugger;
+    let textRow = document.querySelector("#myText");
 
+    const textEvent = {
+        time: moment(),
+        planForTime: textRow.value
+    };
 
+    let K = time + textEvent.planForTime;
+    window.localStorage.setItem(K, JSON.stringify(textEvent));
 
+    const timeT = localStorage.getItem(K);
+    console.log(JSON.parse(timeT));
+    console.log(textEvent);
 
-
-// var threeColumnsTable = document.createElement("table");
-
-// threeColumnsTable.setAttribute("hours", "8");
-// threeColumnsTable.setAttribute("id", "8");
-// threeColumnsTable.setAttribute("personName", "My Name");
-// let tableContainer = document.querySelector(".container");
-// let tableContainer$ = $(".container")
-
-// threeColumnsTable.classList.add("table");
-
-// var hoursTabl = document.createElement("tr");
-// var contentTabl = document.createElement("tr");
-// var saveTabl = document.createElement("tr");
-
-// columnTabl.appendChild(hoursTabl);
-// columnTabl.appendChild(contentTabl);
-// columnTabl.appendChild(saveTabl);
+     
+}
 
 
-// function viewScore() {
-//     //delete the table if it exists
-//     var myTbl = document.getElementById("tableResults");
-//     if (myTbl != null) {
-//       myTbl.remove();
-//     }
-    
-//     var scoreTabl = document.createElement("table");
-  
-  
-//     scoreTabl.setAttribute("id", "tableResults");
-  
-//     let scoreContainer = document.querySelector("#score-container");
-  
-//     scoreTabl.classList.add("table");
-//     var headerTabl = document.createElement("tr");
-//     var scoreTh = document.createElement("th");
-//     scoreTh.innerHTML = "Score: ";
-//     var nameTh = document.createElement("th");
-//     nameTh.innerHTML = "Name: ";
-  
-//     headerTabl.appendChild(scoreTh);
-//     headerTabl.appendChild(nameTh);
-//     scoreTabl.appendChild(headerTabl);
-  
-//     scoreContainer.appendChild(scoreTabl);
-  
-  
-//     for (let i = 0; i < localStorage.length; i++) {
-//       let storedValue = localStorage.key(i);
-  
-//       if (storedValue.includes("user_")) {
-  
-//         // get item from local storage and convert it to javascript object
-//         // call render row passing table and javascript object
-//         let jsonRes = localStorage.getItem(storedValue);
-//         let personRes = JSON.parse(jsonRes);
-  
-//         renderRow(scoreTabl, personRes);
-  
-//       }
-//       console.log(`Item at ${i}: ${storedValue}`);
-//     }
-  
-//   }
-  
-// function renderRow(tblEl, clicker) {
-//     var row = document.createElement("tr");
-//     var scoreColumn = document.createElement("td");
-//     scoreColumn.innerHTML = clicker.scoring;
-  
-//     var nameColumn = document.createElement("td");
-//     nameColumn.innerHTML = clicker.name;
-  
-//     row.appendChild(scoreColumn);
-//     row.appendChild(nameColumn);
-//     tblEl.appendChild(row);
-//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
